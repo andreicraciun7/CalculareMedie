96657dc5451665bd80524e5a2d108cc28e0d2a8a
Andrew <andrei.craciun97@yahoo.com>
2023-03-24 18:26:59 +0200

    up

--- main.py
+++ main.py
@@ -28,16 +28,32 @@ while True:
     lista_note_optional.append(nota_optional)
 
 while True:
-    verifica_media = input("\n Introduceți numele unei persoane pentru a vedea media notelor și situația promovării.\nPentru a termina procesul, tastați -done-.")
+    verifica_media = input("\n Introduceți numele unei persoane pentru a vedea media notelor și situația promovării.\n Pentru a termina procesul, tastați -done-.")
     if verifica_media == "done":
         break
-    if verifica_media in lista_nume:
+    if verifica_media not in lista_nume:
+        print("Persoana nu se află pe listă.")
+    else:
         x = int(lista_nume.index(verifica_media))
         media_notelor = (lista_note_matematica[x] + lista_note_romana[x] + lista_note_optional[x]) / 3
-        print(media_notelor)
-        if media_notelor >= 6:
-            print("Promovat!")
+        y = round(media_notelor, 2)
+        print("Media obținută este: " + str(y))
+        if media_notelor < 6:
+            print("Picat. Media de promovare este minim 6.")
         else:
-            print("Picat!")
-    else:
-        print ("Persoana nu se află pe listă.")
+            if lista_note_matematica[x] >= 5:
+                pass
+            else:
+                print("Picat. Nota minima de trecere este 5.")
+                break
+            if lista_note_romana[x] >= 5:
+                pass
+            else:
+                print("Picat. Nota minima de trecere este 5.")
+                break
+            if lista_note_optional[x] >= 5:
+                pass
+            else:
+                print("Picat. Nota minima de trecere este 5.")
+                break
+            print("Promovat.")
